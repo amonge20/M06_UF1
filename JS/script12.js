6b0bff86b78377992ccf01a01f2ce87eb853cf83
amonge20 <2023_aitor.monge@iticbcn.cat>
2023-10-16 19:57:56 +0200

    Exercicis de Matrius

--- JS/script12.js
+++ JS/script12.js
@@ -1,11 +1,26 @@
 function tabla() {
     let filas = parseInt(prompt("Número de filas: "));
-    let columnas = filas * 2 - 1; // El número de columnas es igual al doble del número de filas menos 1
+    let columnas = filas * 2 - 1;
 
     const taula = document.getElementById("taula");
     taula.innerHTML = "";
 
     for (let i = 0; i < filas; i++) {
+        const row = document.createElement("tr");
+        
+        for (let j = 0; j < columnas; j++) {
+            const cell = document.createElement("td");
+            cell.style.backgroundColor = "white";
+
+            if (j >= filas - i - 1 && j < filas + i) {
+                cell.style.backgroundColor = "red";
+            }
+            row.appendChild(cell);
+        }
+        taula.appendChild(row);
+    }
+
+    for (let i = filas - 2; i >= 0; i--) {
         const row = document.createElement("tr");
         
         for (let j = 0; j < columnas; j++) {
