ba17742ff37f224bd302bb267347c7790f896726
amonge20 <2023_aitor.monge@iticbcn.cat>
2023-10-25 20:46:14 +0200

    Exercicis nous BOM v1.5

--- JS/script17.js
+++ JS/script17.js
@@ -1,4 +1,4 @@
-function letra3(){
+function letra3(){ //en casa
     var texto = prompt("Escribe una palabra: ");
     var caracter = prompt("Caracter que quieres eliminar: ");
     var posicion = prompt("Elige el nº de posición: ");
--- JS/script18.js
+++ JS/script18.js
@@ -1,12 +1,30 @@
-function todoJunto(){
+function todoJunto(){ //EN CASA
+    //TEXT EN MAJUSCULES I MINUSCULES
     var textoTodo = prompt("Escriu un text: ");
-
     document.write(textoTodo.toUpperCase() + "\n");
-
     document.write(textoTodo.toLowerCase() + "\n");
 
+    //LONGITUD DEL TEXT
     var longitudTexto = textoTodo.length - 1;
-
     document.write(" tiene " + longitudTexto + " Caracteres" + "\n");
 
+    //SEPARA LES PARAULES DEL TEXT
+    var separaPalabras = " - ";
+    document.write("Separando letras: " + textoTodo.split(separaPalabras) + "\n");
+    
+    //VOCALS QUE CONTÉ 
+    var vocales = textoTodo["a","e","i","o","u"];
+    var cuentaVocales = vocales.length - 1;
+
+    document.write("Contiene " + cuentaVocales);
+
+    //NUMERO DE VOCALS QUE CONTÉ EL TEXT
+
+
+    //LA FRASE EN Nº DE LINES PERO PER CADA LINEA LI FALTA UNA LLETRA AL DAVANT
+    var saltoLinea = textoTodo.split("\n");
+    for (var i = 0; i < saltoLinea.length; i++){
+        document.write(saltoLinea[i].substring(1) + "\n");
+        saltoLinea++;
+    }
 }
