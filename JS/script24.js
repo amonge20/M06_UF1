ef55ef7c25feb96ba8c485f07a92465d52b4422c
amonge20 <2023_aitor.monge@iticbcn.cat>
2023-10-30 17:21:58 +0100

    ACTIVITATS INICIALS MODIFICACIONS 3.0.01

--- JS/script24.js
+++ JS/script24.js
@@ -1,39 +1,30 @@
-function EuropaIBAN(palabra) {
-    palabra = palabra.toLowerCase();
-    let conteoA = 0;
-    let conteoE = 0;
-    let conteoI = 0;
-    let conteoO = 0;
-    let conteoU = 0;
-    
-    for (let i = 0; i < palabra.length; i++) {
-      switch (palabra[i]) {
-        case 'a':
-          conteoA++;
-          break;
-        case 'e':
-          conteoE++;
-          break;
-        case 'i':
-          conteoI++;
-          break;
-        case 'o':
-          conteoO++;
-          break;
-        case 'u':
-          conteoU++;
-          break;
-      }
+function CodigoDeBarras(barcode) {
+    let odd = 0;
+    let even = 0;
+
+    for (let i = barcode.length - 2; i >= 0; i -= 2) {
+        odd += parseInt(barcode[i]);
     }
-    
-    return (conteoA > 0 && conteoE > 0 && conteoI > 0 && conteoO > 0 && conteoU > 0);
-  }
-  
-  const palabra = prompt("Introduce una palabra:"); // Solicitar entrada al usuario
-  
-  if (EuropaIBAN(palabra)) {
-    console.log("La palabra es pentavocálica.");
-  } else {
-    console.log("La palabra no es pentavocálica.");
-  }
-  
+
+    for (let i = barcode.length - 1; i >= 0; i -= 2) {
+        even += parseInt(barcode[i]);
+    }
+
+    let total = odd + even * 3;
+    let check = (10 - (total % 10)) % 10;
+    return check;
+}
+
+function checkDigit(barcode) {
+    let check = CodigoDeBarras(barcode);
+    return check;
+}
+
+let barcode = prompt("Introduïu el codi de barres (EAN8 o EAN13):");
+
+if (barcode.length === 8 || barcode.length === 13) {
+    let check = checkDigit(barcode);
+    console.log("El dígit de control calculat és: " + check);
+} else {
+    console.log("El codi de barres introduït no té la longitud correcta (8 o 13 dígits).");
+}
